3c31567005e30e894f431d00fa38776c509080b9
vaishnavim1121 <vaishnavim1121@gmail.com>
2026-02-25 14:56:39 +0530

    initial commit

---
+++ src/main/java/com/example/MainClass.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class MainClass {
+    public static void main(String[] args) {
+        // We use System.out.println to print to the console
+        System.out.println("HelloClass, happy to see you all");
+    }
+}
